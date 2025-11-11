6994940567562430854fc311fd64578927c3b81f
jollyroyy <70683975+jollyroyy@users.noreply.github.com>
2025-11-11 11:40:56 +0530

    Updated navbar-with-dropdowns.tsx

--- src/components/ui/navbar-with-dropdowns.tsx
+++ src/components/ui/navbar-with-dropdowns.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { LucideIcon } from "lucide-react";
+import { Video as LucideIcon } from "lucide-react";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 
